8630cc3db541229cdd2373c2343a04b053077df8
Benjamin Looi <benjaminlooidt@gmail.com>
2021-04-22 04:28:46 +0800

    Finish resume page

---
+++ pages/resume.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto">
+    <img
+      class="w-full"
+      :src="require('~/assets/img/cv.jpg')"
+      alt="Benjamin Looi's resume"
+    />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
