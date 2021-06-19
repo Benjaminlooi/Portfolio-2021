243dd4599d175148d2f6bdef59731bbe0278d15a
Benjamin Looi <benjaminlooidt@gmail.com>
2021-06-19 22:46:16 +0800

    Improve design

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="root" class="container mx-auto">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
       <div
         v-for="(project, i) in projectsData"
         :key="i"
@@ -13,16 +13,17 @@
               :alt="project.imageAlt"
             />
           </a>
-          <div class="px-4 py-2 mt-2 bg-white">
-            <h2 class="font-bold text-2xl text-gray-800">
+
+          <div class="p-4 bg-white">
+            <h2 class="font-bold text-2xl text-gray-800 flex items-center">
               {{ project.projectTitle }}
               <a
                 :href="project.projectLink"
-                class="text-base text-blue-400 hover:text-blue-500"
+                class="text-base text-blue-400 hover:text-blue-500 ml-1.5"
                 ><font-awesome-icon icon="external-link-alt"
               /></a>
             </h2>
-            <p class="sm:text-sm text-xs text-gray-700 px-2 mr-1 my-3">
+            <p class="sm:text-sm text-xs text-gray-700 mt-1">
               <span v-html="project.projectDesc"></span>
             </p>
           </div>
